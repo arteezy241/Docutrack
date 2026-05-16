fb73d4549249de9992459f7eb765376100619e4d
arteezy241 <arteezy241@gmail.com>
2026-05-16 21:37:35 +0800

    Sync all models migration

---
+++ DocuTrack.Infrastructure/Migrations/20260516133701_SyncAllModels.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocuTrack.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncAllModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
